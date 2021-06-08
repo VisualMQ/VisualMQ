735042c9585dcd7be82873bdf67a23f438864df7
Lukas Cerny <cerny.lukas.oto@gmail.com>
2021-06-08 09:43:58 +0200

    Queue, QMs and Channel data.

--- python_rest_api.py
+++ python_rest_api.py
@@ -1,10 +1,11 @@
 import requests
+import json
 from requests.auth import HTTPBasicAuth
 
 
 HOST = "localhost"
 PORT = "9443"
-BASE = "https://{}:{}/ibmmq/rest/v2/admin".format(HOST, PORT)
+BASE = "https://{}:{}/ibmmq/rest/v1/admin".format(HOST, PORT)
 
 def getRequest(endpoint):
     # We do not verify SSL certificate because on localhost they are self-signed
@@ -17,24 +18,26 @@ def getQueueManagers():
     data = getRequest("/qmgr")
     return data.get("qmgr")
 
-
 # See https://www.ibm.com/docs/en/ibm-mq/9.2?topic=adminqmgr-get
 def getQueueManager(qmgr):
     data = getRequest("/qmgr/{}?attributes=*".format(qmgr))
     return data.get("qmgr")
 
-# For some reason not working, we might need cloud instance see https://stackoverflow.com/questions/57179975/in-ibm-mq-java-how-do-i-access-all-the-queues-under-a-given-queue-manager-pref
+# See https://www.ibm.com/docs/en/ibm-mq/9.0?topic=resources-adminqmgrqmgrnamequeue
 def getQueues(qmgr):
     data = getRequest("/qmgr/{}/queue?type=all&attributes=*".format(qmgr))
-    print(data)
     return data.get("queue")
 
-def getQueue(queue):
-    data = getRequest("/queue/{}?attributes=*".format(queue))
+def getQueue(qmgr, queue):
+    data = getRequest("/qmgr/{}/queue/{}?type=all&attributes=*".format(qmgr, queue))
     return data.get("queue")
 
-print(getQueueManagers())
-print(getQueueManager("QM1"))
-print(getQueues("QM1"))
-# print(getQueueManager("QM1"))
+# See https://www.ibm.com/docs/en/ibm-mq/9.0?topic=adminqmgrqmgrnamechannel-get#q130500___examples
+def getChannels(qmgr):
+    data = getRequest("/qmgr/{}/channel?attributes=*".format(qmgr))
+    return data.get("channel")
 
+
+
+# print(getQueue("QM1", "DEV.QUEUE.1"))
+print(json.dumps(getChannels("QM1")[0]))
