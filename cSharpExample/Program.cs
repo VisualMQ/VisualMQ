cc2802b54c53ce7551a1db53a63af4a70e3cd301
Shucheng Tian <shucheng.tian@outlook.com>
2021-06-14 17:59:44 +0800

    fixed endpoint issue (502)

--- cSharpExample/Program.cs
+++ cSharpExample/Program.cs
@@ -7,7 +7,7 @@ using System.Net.Http.Headers;
 namespace ConsoleProgram {
     public class RESTMQ {
         //This BASE endpoint is for Queue Manager 1
-        private const string BASE = "https://web-qm1-3628.qm.eu-gb.mq.appdomain.cloud:443/ibmmq/rest/v1";
+        private const string BASE = "https://web-qm1-3628.qm.eu-gb.mq.appdomain.cloud:443";
         
         public static HttpClient Authenticate(string username, string apikey) {
             HttpClient client = new HttpClient();
@@ -15,16 +15,18 @@ namespace ConsoleProgram {
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             //Post request to the Login endpoint, body includes username and password(apikey)
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "/login");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "/ibmmq/rest/v1/login");
             string body = "{\"username\":\"" + username + "\",\"password\":\"" + apikey + "\"}";
             request.Content = new StringContent(body, Encoding.UTF8, "application/json");
   
             HttpResponseMessage response = client.SendAsync(request).Result;
+            
             return client;
         }
 
+        //For all utility funtions, pass client by reference.
         public static string GetAllChannels(ref HttpClient client, string qmgr) {
-            HttpResponseMessage response = client.GetAsync("/admin/qmgr/" + qmgr + "/channel").Result;
+            HttpResponseMessage response = client.GetAsync("/ibmmq/rest/v1/admin/qmgr/" + qmgr + "/channel").Result;
             string responseBody = response.Content.ReadAsStringAsync().Result;
             return responseBody;
         }
@@ -37,13 +39,9 @@ namespace ConsoleProgram {
             //First Authenticate with the IBM MQ on Cloud
             HttpClient client = RESTMQ.Authenticate("shuchengtian", "aCPHZ4ys0Tnn2xQLPsHc6lEz4CTenKmNsyW9q0MoQ0bf"); //hard-coded for now
 
-            //Perform any Rest API calls below
-            // string all_channels = RESTMQ.GetAllChannels(ref client, QM1);
-            // Console.WriteLine("{0}", all_channels);
-
-            HttpResponseMessage response = client.GetAsync("/admin/qmgr/QM1/channel").Result;
-            string responseBody = response.Content.ReadAsStringAsync().Result;
-            Console.WriteLine("{0}", responseBody);
+            // Perform any Rest API calls below
+            string all_channels = RESTMQ.GetAllChannels(ref client, QM1);
+            Console.WriteLine("{0}", all_channels);
 
             //Close client connection
             client.Dispose();
