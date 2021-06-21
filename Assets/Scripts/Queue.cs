b46c4161c72655d36edbf53f730cc30cfc528d5a
yuexu <jessicaxu0516@gmail.com>
2021-06-21 10:25:17 +0100

    Revert "Merge branch 'main' into filter"
    This reverts commit 70808a05a69653dddb1186bc3ef8e929e0f90ec1, reversing
    changes made to 9a75cf06d6d1d293cdc6ac41fcf4113f739deae5.

--- Assets/Scripts/Queue.cs
+++ Assets/Scripts/Queue.cs
@@ -9,7 +9,15 @@ public class Queue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Script is working...");
+        string testUsername = "YOUR_USERNAME";
+        string testAPIKey = "YOUR_API_KEY";
+        string testQMUrl = "https://web-qm1-3628.qm.eu-gb.mq.appdomain.cloud:443";
+        string testQmgr = "QM1";
 
+        QueueManager queue_manager = new QueueManager(testQMUrl, testQmgr, testUsername, testAPIKey);
+        string allQueues = queue_manager.GetAllQueues();
+        Debug.Log(allQueues);
     }
 
     // Update is called once per frame
--- README.md
+++ README.md
@@ -5,5 +5,3 @@
 1. Open the folder in Unity as a new project.
 1. Create and develop :art:
 
-## Others
-branch filter: Filter related code, UX/UI design (@Yue XU), Will merge the main later.
