d648e3a36635456522d01a36b038b6b52af5f7ad
Diallo <contact@diallom.com>
2021-07-02 02:41:08 +0100

    Modified rendering script to receive info directly from internal state

--- Assets/Scripts/Rendering/CreateVisual.cs
+++ Assets/Scripts/Rendering/CreateVisual.cs
@@ -3,40 +3,130 @@ using System.Collections.Generic;
 using UnityEngine;
 using MQ;
 public static class CreateVisual 
-{
+{   
+    public static Dictionary<string, QMRender> renderedQMs = new Dictionary<string, QMRender>();
+    public static State internalState;
 
-    private static readonly PrimitiveType QMObject = PrimitiveType.Cube;
-    private static readonly PrimitiveType QueueObject = PrimitiveType.Cylinder;
+    public class QMRender{
+        public GameObject renderedObject;
+        public QueueManager QMinfo;
+        public Dictionary<string, QueueRender> renderedQueues;
 
-    private static Dictionary<string, int> numberQueues = new Dictionary<string, int>();
-    private static int numberManagers = 0;
+        public QMRender(QueueManager QMinfo, GameObject renderedObject){
+            this.renderedObject = renderedObject;
+            this.QMinfo = QMinfo;
+            this.renderedQueues = new Dictionary<string, QueueRender>();
 
-    // Temporary:
-    static int QMXVALUE = 15;
-    public static void VisualizeQM(QMInfo manager)
+        }
+        
+
+    }
+    public class QueueRender{
+
+        public QMRender parentQM;
+        public Queue queueInfo;
+        public GameObject renderedObject;
+        public int positionRank; // TODO later
+
+        public QueueRender(QMRender parentQM, Queue queueInfo, GameObject renderedObject){
+            this.parentQM = parentQM;
+            this.queueInfo = queueInfo;
+            this.renderedObject = renderedObject;
+        }
+    }
+
+
+    public static void VisualizeQM(QueueManager manager)
     {   
         GameObject managerObj = GameObject.CreatePrimitive(QMObject);
         managerObj.transform.position = new Vector3(numberManagers * QMXVALUE, 0, 0); // Side by side
         managerObj.transform.localScale = new Vector3(10, 0.2f, 20);
-        manager.obj = managerObj;
-        // TODO: hardcoded values here change when prefabs created
+
+        /* Now we add the render */
+        QMRender qmrender = new QMRender(manager, managerObj);
+        renderedQMs[manager.QMInfo.name] = qmrender; // We assume it is unique
 
         numberManagers++;
     }
+    // Delete function here
+    
+    
 
-
-    public static void VisualizeQueue(QMInfo manager, QueueStorage queue)
+    public static void VisualizeQueue(QueueManager manager, Queue queueInfo)
     {
         GameObject queueObj = GameObject.CreatePrimitive(QueueObject);
         queueObj.transform.parent = manager.obj.transform;
         queueObj.transform.localPosition = new Vector3(0.0f, 5.33f, (-0.45f) + 0.1f * (numberQueues.ContainsKey(manager.name) ? numberQueues[manager.name] : 0));
         queueObj.transform.localScale = new Vector3(0.1f, 5.0f, 0.05f);
-        queue.obj = queueObj;
+        /* Now add the render */
+        // THIS SHOULD NEVER GO WRONG ENSURE THIS:
+        QMRender parentQM = renderedQMs[manager.QMInfo.name]
+        QueueRender queuerender = new QueueRender(parentQM, queueInfo, queueObj)
+        
 
-        Increment(numberQueues, manager.name);
+        Increment(numberQueues, manager.QMInfo.name);
     }
 
-       
+    public static void generate_render(){
+        /* ACCESS INTERNAL STATE HERE */
+
+        foreach (QueueManager manager in internalState.QMs)
+        {
+            if(!renderedQMs.ContainsKey(manager.QMInfo.name)){
+                VisualizeQM(manager);
+            }
+            QMRender qmrender = renderedQMs[manager.QMInfo.name];
+            
+            foreach (Queue queue in manager.Queues)
+            {
+                if(!qmrender.renderedQueues.ContainsKey(queue.name)){
+                    VisualizeQueue(manager,queue);
+                }
+            }
+        }
+    }
+
+
+    // ///old:
+
+    // private static readonly PrimitiveType QMObject = PrimitiveType.Cube;
+    // private static readonly PrimitiveType QueueObject = PrimitiveType.Cylinder;
+
+    // private static Dictionary<string, int> numberQueues = new Dictionary<string, int>();
+    // private static int numberManagers = 0;
+
+    // // Temporary:
+    // static int QMXVALUE = 15;
+    // public static void VisualizeQM(QMInfo manager)
+    // {   
+    //     GameObject managerObj = GameObject.CreatePrimitive(QMObject);
+    //     managerObj.transform.position = new Vector3(numberManagers * QMXVALUE, 0, 0); // Side by side
+    //     managerObj.transform.localScale = new Vector3(10, 0.2f, 20);
+    //     manager.obj = managerObj;
+    //     // TODO: hardcoded values here change when prefabs created
+
+
+    //     numberManagers++;
+    // }
+
+
+    // public static void VisualizeQueue(QMInfo manager, QueueStorage queue)
+    // {
+    //     GameObject queueObj = GameObject.CreatePrimitive(QueueObject);
+    //     queueObj.transform.parent = manager.obj.transform;
+    //     queueObj.transform.localPosition = new Vector3(0.0f, 5.33f, (-0.45f) + 0.1f * (numberQueues.ContainsKey(manager.name) ? numberQueues[manager.name] : 0));
+    //     queueObj.transform.localScale = new Vector3(0.1f, 5.0f, 0.05f);
+    //     queue.obj = queueObj;
+
+    //     Increment(numberQueues, manager.name);
+    // }
+
+
+    // public static void generate_render(){
+
+    // }
+
+    // Helper function   
     public static void Increment<T>(this Dictionary<T, int> dictionary, T key)
     {
         int count = 0;
