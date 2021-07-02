5e7afd72fa9be969b3b08e7634cf5287b3aa8cfd
Diallo <contact@diallom.com>
2021-07-02 02:46:35 +0100

    Added functionality for rendering from internal state

--- Assets/Scripts/Rendering/RegularReader.cs
+++ Assets/Scripts/Rendering/RegularReader.cs
@@ -17,34 +17,61 @@ public class RegularReader : MonoBehaviour
 
     void Start()
     {
-        //Step one: create an Http client (QueueManager class), remember to set the username and apikey
-        string testUsername = "NA";
-        string testAPIKey = "NA";
-        string testQMUrl = "NA";
+        //Step one: create an Http client (QMClient class), remember to set the username and apikey
+        string testUsername = "shuchengtian";
+        string testAPIKey = "aCPHZ4ys0Tnn2xQLPsHc6lEz4CTenKmNsyW9q0MoQ0bf";
+        string testQMUrl = "https://web-qm1-3628.qm.eu-gb.mq.appdomain.cloud:443";
         string testQmgr = "QM1";
-        QueueManager mq = new QueueManager(testQMUrl, testQmgr, testUsername, testAPIKey);
 
-        //Step two: create QMInfo, QueuesInfo, and List of MessagesInfo objects
+        //Step one+: create a second Http client
+        QMClient mq, mq2;
+        string testQMUrl2 = "https://web-qm2-3628.qm.eu-gb.mq.appdomain.cloud:443";
+        string testQmgr2 = "qm2";
+        try {
+            mq = new QMClient(testQMUrl, testQmgr, testUsername, testAPIKey);
+            mq2 = new QMClient(testQMUrl2, testQmgr2, testUsername, testAPIKey);
+        } catch (Exception) {
+            Console.WriteLine("Authentication failed.");
+            return;
+        }
+
+        //Step two: create QMInfo, QueuesFactory(for making queues), and inset MessagesInfo objects to each queue
         string QMInfo = mq.GetQmgr();
         QMInfo QM1 = new QMInfo(QMInfo);
 
+        string QMInfo2 = mq2.GetQmgr();
+        QMInfo QM2 = new QMInfo(QMInfo2);
+
         string allqueue = mq.GetAllQueues();
-        QueuesInfo Qs1 = new QueuesInfo(allqueue);
+        QueuesFactory Qs1 = new QueuesFactory();
+        List<Queue> Queues1 = Qs1.makeQueues(allqueue);
 
-        List<MessagesInfo> allQMessages = new List<MessagesInfo>();
-        
+        string allqueue2 = mq2.GetAllQueues();
+        QueuesFactory Qs2 = new QueuesFactory();
+        List<Queue> Queues2 = Qs2.makeQueues(allqueue2);
 
-        //Step three: create a state object that consists of the three objects/list of objects in step three
-        internalState = new State(QM1, Qs1, allQMessages);
-
-        // Display this current QM1
-
-        CreateVisual.VisualizeQM(QM1);
-
-        foreach(QueueStorage storage in internalState.QueuesInfo.storage)
-        {
-            CreateVisual.VisualizeQueue(QM1, storage);
+        for (int i = 0; i < Queues1.Count; i++) {
+            string messageInfo = mq.GetAllMessageIds(Queues1[i].name);
+            Queues1[i].messages = new MessagesInfo(Queues1[i].name, messageInfo);
         }
+
+        for (int i = 0; i < Queues2.Count; i++) {
+            string messageInfo = mq2.GetAllMessageIds(Queues2[i].name);
+            Queues2[i].messages = new MessagesInfo(Queues2[i].name, messageInfo);
+        }
+        
+        //Step three: create a QueueManager object that consists QMinfo and list of queues
+        //Maybe store the QMClient in the QueueManager Object as well?
+        QueueManager QueueManager1 = new QueueManager(QM1, Queues1);
+        QueueManager QueueManager2 = new QueueManager(QM2, Queues2);
+        
+        //Step four: create a list of QueueManagers and create an internal state object
+        List<QueueManager> qms = new List<QueueManager>();
+        qms.Add(QueueManager1);
+        qms.Add(QueueManager2);
+        internalState = new State(qms);
+
+        
 
 
 
@@ -55,41 +82,6 @@ public class RegularReader : MonoBehaviour
     // Update is called once per frame
     void ContinousRender()
     {
-        /*Have to iterate over this here when the proper internal state has been generated*/
-        QMInfo qm1 = internalState.QMInfo;
-
-        /* Two things we have to do within the loop body: FIRST we need to render any new objects. */
-        if (qm1.obj == null && renderedQMs.ContainsKey(qm1.name)){
-            /* IF the object is null it has NOT been rendered. HOWEVER its also in the internal state
-             therefore we must render this new QM.
-             assumption: QM names are unique.
-             */
-
-            CreateVisual.VisualizeQM(qm1);
-            renderedQMs.Add(qm1.name, true);
-        }
-
-        for(int i = 0; i < internalState.QMInfo.name.Length; i++){
-           string currName = internalState.QueuesInfo.name[i];
-           QueueStorage currStorage = internalState.QueuesInfo.storage[i];
-
-            /*
-             * Same checks as above. See if this is not NULL and within the dict
-             */
-
-            if (currStorage.obj == null && renderedQueues.ContainsKey(currName))
-            {
-                /* IF the object is null it has NOT been rendered. HOWEVER its also in the internal state
-                 therefore we must render this new QM.
-                 assumption: QM names are unique.
-                 */
-
-                CreateVisual.VisualizeQueue(qm1,currStorage);
-                renderedQueues.Add(currName, true);
-            }
-        }
-
-        /** Helpers for testing TODO: We need a proper update function **/
-
+        CreateVisual.generate_render();
     }
 }
