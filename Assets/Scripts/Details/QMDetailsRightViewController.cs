6bc3a272b904a37c905037467b20241bb3b09688
yuexu <jessicaxu0516@gmail.com>
2021-07-07 14:55:14 +0100

    Add queue lists

--- Assets/Scripts/Details/QMDetailsRightViewController.cs
+++ Assets/Scripts/Details/QMDetailsRightViewController.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class QMDetailsRightViewController : MonoBehaviour
 {
+    private Transform QueueRowItem;
+    private Transform container;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        container = transform.Find("QueueRowContainer");
+        QueueRowItem = container.Find("QueueRowItem");
         
+        QueueRowItem.gameObject.SetActive(false);
+        float rowHeight = 45f;
+        float startY = 0f;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Transform item = Instantiate(QueueRowItem, container);
+            RectTransform recTransform = item.GetComponent<RectTransform>();
+            recTransform.anchoredPosition = new Vector2(-3, -rowHeight * i+50);
+            item.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
