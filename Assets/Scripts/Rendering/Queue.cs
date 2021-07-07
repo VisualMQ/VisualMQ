6bec29e4d0f1fa3b2086bbfb6ad7778b6a4708aa
Diallo <mnl.diallo@gmail.com>
2021-07-07 13:28:09 +0100

    Added ability to hide/display Queues based upon Euclidean distance

--- Assets/Scripts/Rendering/Queue.cs
+++ Assets/Scripts/Rendering/Queue.cs
@@ -7,7 +7,9 @@ public class Queue : MonoBehaviour
 
     public Vector3 position;
     public MQ.Queue queue;
-    public Transform textMeshTransform;
+ 
+    public TextMesh textMesh;
+    public QueueManager parent;
 
     // Use this for initialization
     void Start()
@@ -37,23 +39,46 @@ public class Queue : MonoBehaviour
         GameObject instantiatedQueue = Instantiate(queuePrefab, position, Quaternion.identity) as GameObject;
         instantiatedQueue.transform.parent = this.transform;
 
+        
+
         // TODO: Move this to prefab
+        // Generate the initial text to be displayed above Queues.
         GameObject textObj = new GameObject();
         textObj.transform.parent = instantiatedQueue.transform;
 
-        TextMesh textMesh = textObj.AddComponent<TextMesh>();
+        textMesh = textObj.AddComponent<TextMesh>();
         textMesh.text = queue.queueName;
-        textMesh.characterSize = 0.4f;
         textMesh.anchor = TextAnchor.MiddleCenter;
         textMesh.alignment = TextAlignment.Center;
         textMesh.transform.position = new Vector3(instantiatedQueue.transform.position.x, instantiatedQueue.transform.position.y + 5, instantiatedQueue.transform.position.z);
-        textMeshTransform = textMesh.transform;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        textMeshTransform.rotation = Quaternion.LookRotation(textMeshTransform.position - Camera.main.transform.position);
+
+        // Magic number (TODO) currently hides if euclidean distance between camera and text.
+        if(Vector3.Distance(this.position, Camera.main.transform.position) < 35)
+        {
+            textMesh.gameObject.SetActive(true);
+        }
+        else
+        {
+            textMesh.gameObject.SetActive(false);
+        }
+
+        // Change the size log decreasing towards min of 0.2.
+        textMesh.characterSize = (0.5f / parent.queues.Count) + 0.2f;
+
+        // Obtain the first Queue component and align all the text according to it.
+        var firstIndex = parent.renderedQueues.GetEnumerator();
+        firstIndex.MoveNext();
+        Queue firstQueue = firstIndex.Current.Value.GetComponent(typeof(Queue)) as Queue;
+
+        textMesh.transform.rotation = Quaternion.LookRotation(firstQueue.textMesh.transform.position - Camera.main.transform.position);
+
+
+
     }
 }
