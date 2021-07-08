869bd034a0d14458a176d85e4a26963583aefa1c
Lukas Cerny <cerny.lukas.oto@gmail.com>
2021-07-08 19:40:06 +0200

    commit.

--- Assets/Scripts/Rendering/QueueManager.cs
+++ Assets/Scripts/Rendering/QueueManager.cs
@@ -7,12 +7,16 @@ using UnityEngine;
 public class QueueManager : MonoBehaviour
 {
 
+    private GameObject blockPrefab;
+
+    // Scale factor of blocks
+    private const float sXZ = 4f;
+    private const float sY = 0.1286252f * 2;
+
     public List<MQ.Queue> queues;
     private Dictionary<string, GameObject> renderedQueues = new Dictionary<string, GameObject>();
 
-    private GameObject blockPrefab;
-
-
+    
     // Unity calls this method at the complete beginning, even before Start
     void Awake()
     {
@@ -24,43 +28,89 @@ public class QueueManager : MonoBehaviour
     {
         Debug.Log("Rendering " + queues.Count + " queues.");
 
-        // Render Qmgr plane from blocks
-        //for (int x = 0; x < queues.Count; ++x)
-        //{
-        //    Instantiate(blockPrefab, new Vector3(2.5f * x, 0, 0), Quaternion.identity);
-        //    block.transform.parent = this.transform;
-        //}
+        // Compute how many queues are of each type
         Dictionary<string, int> numberOfQueues = new Dictionary<string, int>();
+        numberOfQueues[MQ.AliasQueue.typeName] = 0;
+        numberOfQueues[MQ.RemoteQueue.typeName] = 0;
+        numberOfQueues[MQ.TransmissionQueue.typeName] = 0;
+        numberOfQueues[MQ.LocalQueue.typeName] = 0;
         foreach (MQ.Queue queue in queues)
         {
-            if (queue is MQ.LocalQueue) numberOfQueues["local"]++;
-            else if (queue is MQ.TransmissionQueue) numberOfQueues["transmission"]++;
-            else if (queue is MQ.RemoteQueue) numberOfQueues["remote"]++;
-            else if (queue is MQ.AliasQueue) numberOfQueues["alias"]++;
-
+            numberOfQueues[queue.GetTypeName()]++;
         }
 
+        // We will have 2 large rectangle areas and 2 small rectangle areas
+        List<KeyValuePair<string, int>> numberOfQueuesList = numberOfQueues.ToList();
+        numberOfQueuesList.Sort((x, y) => x.Value.CompareTo(y.Value));
 
+        KeyValuePair<string, int> highestQueues = numberOfQueuesList[3];
+        KeyValuePair<string, int> secondSmallestQueues = numberOfQueuesList[1];
+
+        int[] largeArea = ComputeRectangleArea(highestQueues.Value);
+        int[] smallArea = ComputeRectangleArea(secondSmallestQueues.Value, largeArea[1]);
+        Debug.Assert(largeArea[1] == smallArea[0]);
+        Debug.Log("large area dimensions " + largeArea[0] + " " + largeArea[1]);
+        Debug.Log("small area dimensions " + smallArea[0] + " " + smallArea[1]);
+
+
+        Dictionary<string, Vector3> offsets = new Dictionary<string, Vector3>();
+        Dictionary<string, int[]> dimensions = new Dictionary<string, int[]>();
+        offsets[numberOfQueuesList[3].Key] = new Vector3(0, 0, 0);
+        offsets[numberOfQueuesList[2].Key] = new Vector3(0, 0, sXZ * largeArea[1]);
+        offsets[numberOfQueuesList[1].Key] = new Vector3(sXZ * largeArea[0], 0, 0);
+        offsets[numberOfQueuesList[0].Key] = new Vector3(sXZ * largeArea[0], 0, sXZ * largeArea[1]);
+        dimensions[numberOfQueuesList[3].Key] = largeArea;
+        dimensions[numberOfQueuesList[2].Key] = largeArea;
+        dimensions[numberOfQueuesList[1].Key] = smallArea;
+        dimensions[numberOfQueuesList[0].Key] = smallArea;
+
+        // Render 2 large areas on top of each other
+        for (int x = 0; x < largeArea[0]; x++)
+        {
+            for (int z = 0; z < largeArea[1]; z++)
+            {
+                // Large area
+                GameObject lowerBlock = Instantiate(blockPrefab, new Vector3(sXZ * x, 0, sXZ * z), Quaternion.identity);
+                lowerBlock.transform.parent = this.transform;
+
+                // Large area
+                GameObject upperBlock = Instantiate(blockPrefab, new Vector3(sXZ * x, 0, sXZ * z) + offsets[numberOfQueuesList[2].Key], Quaternion.identity);
+                upperBlock.transform.parent = this.transform;
+            }
+        }
+        // Render 2 small areas
+        for (int x = 0; x < smallArea[1]; x++)
+        {
+            for (int z = 0; z < smallArea[0]; z++)
+            {
+                // Small area
+                GameObject lowerBlock = Instantiate(blockPrefab, new Vector3(sXZ * x, 0, sXZ * z) + offsets[numberOfQueuesList[1].Key], Quaternion.identity);
+                lowerBlock.transform.parent = this.transform;
+
+                // Small area
+                GameObject upperBlock = Instantiate(blockPrefab, new Vector3(sXZ * x, 0, sXZ * z) + offsets[numberOfQueuesList[0].Key], Quaternion.identity);
+                upperBlock.transform.parent = this.transform;
+            }
+        }
 
         // Render inidividual queues
-        for (int x = 0; x < queues.Count; ++x)
+        Dictionary<string, int> numberOfRenderedQueues = new Dictionary<string, int>();
+        numberOfRenderedQueues[MQ.AliasQueue.typeName] = 0;
+        numberOfRenderedQueues[MQ.RemoteQueue.typeName] = 0;
+        numberOfRenderedQueues[MQ.TransmissionQueue.typeName] = 0;
+        numberOfRenderedQueues[MQ.LocalQueue.typeName] = 0;
+        foreach (MQ.Queue queue in queues)
         {
-            MQ.Queue queue = queues[x];
+            string queueType = queue.GetTypeName();
+            int i = numberOfRenderedQueues[queueType]++;
+            Vector3 offset = offsets[queueType];
+            Vector3 position = new Vector3(sXZ * (i % dimensions[queueType][0]), 0, sXZ * (i / dimensions[queueType][0]));
+            Vector3 queueManagerHeight = new Vector3(0, sY, 0);
+
             GameObject queueGameObject = new GameObject(queue.queueName, typeof(Queue));
             Queue queueComponent = queueGameObject.GetComponent(typeof(Queue)) as Queue;
-            queueComponent.position = new Vector3(2.5f*x, 0.25f, 0);
+            queueComponent.position = offset + position + queueManagerHeight;
             queueComponent.queue = queue;
-            // DELETE LATER: TEST CODE
-            if (queue is MQ.LocalQueue)
-            {
-                Debug.Log(queue.queueName + " currently has " + ((MQ.LocalQueue)queue).currentDepth + " messages.");
-            } else if (queue is MQ.TransmissionQueue)
-            {
-                Debug.Log(queue.queueName + " currently has " + ((MQ.TransmissionQueue)queue).currentDepth + " messages.");
-            }
-            //
-            renderedQueues.Add(queue.queueName, queueGameObject);
-
             queueGameObject.transform.parent = this.transform;
         }
     }
@@ -136,27 +186,7 @@ public class QueueManager : MonoBehaviour
     }
 
 
-    private Dictionary<string, int[]> GetQueueAreaDimensions(Dictionary<string, int> numberOfQueuesOfType)
-    {
-        List<KeyValuePair<string, int>> numberOfQueues = numberOfQueuesOfType.ToList();
-        numberOfQueues.Sort((x, y) => x.Value.CompareTo(y.Value));
-
-        KeyValuePair<string, int> highestQueues = numberOfQueues[3];
-        KeyValuePair<string, int> secondSmallestQueues = numberOfQueues[1];
-
-        int[] largeArea = ComputeRectangleArea(highestQueues.Value);
-        int[] smallArea = ComputeRectangleArea(secondSmallestQueues.Value, largeArea[0]);
-
-        Dictionary<string, int[]> result = new Dictionary<string, int[]>();
-        result[numberOfQueues[3].Key] = largeArea;
-        result[numberOfQueues[2].Key] = largeArea;
-        result[numberOfQueues[1].Key] = smallArea;
-        result[numberOfQueues[0].Key] = smallArea;
-
-        return result;
-    }
-
-
+    // Returns dimensions of a rectangle that can hold N queues
     private int[] ComputeRectangleArea(int N)
     {
         int rows = 1;
@@ -172,15 +202,17 @@ public class QueueManager : MonoBehaviour
 
         // We need extra space for remainder queues and for dynamic updates
         // cols will always be higher than rows, ie cols > rows
-        int[] res = { rows, cols + 1 };
+        int[] res = { cols + 1, rows };
         return res;
     }
 
 
+    // Returns dimensions of a rectangle that can hold N queues but
+    // one size of the rectangle is constrained to rows
     private static int[] ComputeRectangleArea(int N, int rows)
     {
         int cols = N / rows;
-        int[] res = { rows, cols + 1 };
+        int[] res = { Math.Max(rows, cols + 1), Math.Min(rows, cols + 1) };
         return res;
     }
 
