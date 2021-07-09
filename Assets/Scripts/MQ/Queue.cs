6dd46cd6b2542f9a3398cf9ef2d84a0fb0c77d42
Lukas Cerny <cerny.lukas.oto@gmail.com>
2021-07-09 11:32:41 +0200

    Create queue areas and position queues inside them (#25)

--- Assets/Scripts/MQ/Queue.cs
+++ Assets/Scripts/MQ/Queue.cs
@@ -3,7 +3,7 @@
 namespace MQ
 {
 
-    public class Queue
+    public abstract class Queue
     {
         public string queueName;
         public int maxNumberOfMessages;
@@ -15,6 +15,10 @@ namespace MQ
         public string timeAltered;
         public bool holdsMessages{ get; protected set; }
         public int currentDepth;
+
+        public static string typeName { get; protected set; }
+        public abstract string GetTypeName();
+
     }
 
 
@@ -23,10 +27,19 @@ namespace MQ
         public string targetQueueName;
         public string targetQmgrName;
         public string transmissionQueueName;
+
+        public new static string typeName = "Remote";
+
         public RemoteQueue()
         {
             currentDepth = 0;
             holdsMessages = false;
+
+        }
+
+        public override string GetTypeName()
+        {
+            return RemoteQueue.typeName;
         }
 
     }
@@ -34,32 +47,56 @@ namespace MQ
 
     public class TransmissionQueue : Queue
     {
+        public new static string typeName = "Transmission";
+
         //TODO: Assign new message fields when message API is ready
         public TransmissionQueue()
         {
             holdsMessages = true;
+        }
+
+        public override string GetTypeName()
+        {
+            return TransmissionQueue.typeName;
         }
     }
 
 
     public class AliasQueue : Queue
     {
+        public new static string typeName = "Alias";
+
         public string targetQueueName;
+
         public AliasQueue()
         {
             currentDepth = 0;
             holdsMessages = false;
+        }
+
+        public override string GetTypeName()
+        {
+            return AliasQueue.typeName;
         }
     }
 
 
     public class LocalQueue : Queue
     {
+        public new static string typeName = "Local";
+
         //TODO: Assign new message fields when message API is ready
         public LocalQueue()
         {
             holdsMessages = true;
         }
+
+
+        public override string GetTypeName()
+        {
+            return LocalQueue.typeName;
+        }
+
     }
 
 }
