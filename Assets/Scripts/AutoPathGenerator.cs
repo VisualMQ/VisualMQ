3274d26b54ccd8f9a6b48c11db0dffe7b9d7f987
Orange0719 <77682603+Orange0719@users.noreply.github.com>
2021-07-16 17:07:32 +0800

    path teat
    path

---
+++ Assets/Scripts/AutoPathGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathCreation{
+    [RequireComponent(typeof(PathCreator))]
+    public class AutoPathGenerator : MonoBehaviour {
+
+        public bool closedLoop = true;
+        public Transform[] waypoints;
+
+        void Start () {
+            if (waypoints.Length > 0) {
+                // Create a new bezier path from the waypoints.
+                BezierPath bezierPath = new BezierPath (waypoints, closedLoop, PathSpace.xyz);
+                GetComponent<PathCreator> ().bezierPath = bezierPath;
+            }
+        }
+    }
+}
