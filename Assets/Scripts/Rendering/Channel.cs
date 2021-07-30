b0eac0eba810bc13e6ea7fee780691e49c8972e8
Diallo <mnl.diallo@gmail.com>
2021-07-30 01:05:15 +0100

    Added ability to click on channels

--- Assets/Scripts/Rendering/Channel.cs
+++ Assets/Scripts/Rendering/Channel.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
-
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using MQ;
 
 [RequireComponent(typeof(NameRenderer))]
 public class Channel : MonoBehaviour
@@ -30,6 +34,11 @@ public class Channel : MonoBehaviour
         instantiatedChannel = Instantiate(channelPrefab, new Vector3(0, 0, 0), Quaternion.Euler(-90f, 180f, 0f)) as GameObject;
         instantiatedChannel.transform.parent = this.transform;
         instantiatedChannel.transform.parent.position = this.position;
+
+
+        // Add mesh Colider
+        MeshCollider mc = instantiatedChannel.transform.parent.gameObject.AddComponent<MeshCollider>();
+        mc.sharedMesh = instantiatedChannel.GetComponent<MeshFilter>().sharedMesh;
     }
 
     // Update is called once per frame
@@ -37,4 +46,61 @@ public class Channel : MonoBehaviour
     {
 
     }
+
+
+    void OnMouseUp()
+    {
+        // TODO: This code is copy pasted from Queue code. we should change this.
+        Debug.Log("Mouse");
+        // If user clicks on UI objects, Return: Avoid Click through
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+
+
+
+        instantiatedChannel.transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
+        /*Do whatever here as per your need*/
+        GameObject mainCamera = GameObject.Find("Main Camera");
+        mainCamera.transform.rotation = Quaternion.identity;
+        mainCamera.transform.rotation = Quaternion.AngleAxis(70, new Vector3(1, 0, 0));
+        Vector3 targetPosition = this.transform.position;
+        targetPosition.y += 18f;
+        targetPosition.x += 10f;
+        targetPosition.z -= 5f;
+        mainCamera.transform.position = targetPosition;
+        // Camera.main.transform.LookAt(this.position);
+        // Camera.main.transform.position = new Vector3(2.5f, 15f, -13f) + this.position;
+        Debug.Log("Moving Camera to Channel" + this.name);
+
+
+
+
+        var outline = gameObject.GetComponent<Outline>();
+        if (outline == null)
+        {
+            outline = gameObject.AddComponent<Outline>();
+            outline.OutlineMode = Outline.Mode.OutlineAll;
+            outline.OutlineColor = Color.yellow;
+            outline.OutlineWidth = 5f;
+            outline.enabled = false;
+        }
+
+
+        outline.enabled = !outline.enabled;
+    }
+
+
+    void OnMouseEnter()
+    {
+        
+        instantiatedChannel.transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
+    }
+    void OnMouseExit()
+    {
+        instantiatedChannel.transform.localScale = new Vector3(1.00f, 1.00f, 1.00f);
+    }
+
 }
