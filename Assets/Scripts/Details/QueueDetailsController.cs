c18998d60221b44ead3f9ceebef469f7c70aa4c2
yuexu <jessicaxu0516@gmail.com>
2021-08-18 14:28:47 +0100

    Add connection name

--- Assets/Scripts/Details/QueueDetailsController.cs
+++ Assets/Scripts/Details/QueueDetailsController.cs
@@ -22,8 +22,8 @@ public class QueueDetailsController : MonoBehaviour
     private Transform tabButtonsGroups;
     private Button toQueueDetail, toMessageList, toConnections;
 
+    // State & Queue
     private MQ.Queue currentQueue;
-
     private State stateComponent;
 
     private GameObject messageRowTemplate;
