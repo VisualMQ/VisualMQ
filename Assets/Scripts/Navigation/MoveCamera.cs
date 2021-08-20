9a0041b7a3b537a7be08d4fc4e56ec39da248fa8
Lukas Cerny <cerny.lukas.oto@gmail.com>
2021-08-20 09:15:07 +0200

    bug fix: don't move camera when typing.

--- Assets/Scripts/Navigation/MoveCamera.cs
+++ Assets/Scripts/Navigation/MoveCamera.cs
@@ -1,20 +1,18 @@
+/*
+ * Code used from https://gist.github.com/oluf/6951522e4be794178a82cc76be9ee93f
+ * Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
+ * Converted to C# 27-02-13 - no credit wanted.
+ * Simple flycam I made, since I couldn't find any others made public.  
+ * Made simple to use (drag and drop, done) for regular keyboard layout  
+ * WASD : basic movement
+ */
+
 using UnityEngine;
-using System.Collections;
 using UnityEngine.EventSystems;
+
 
 public class MoveCamera : MonoBehaviour
 {
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
-
-    // Docboy add vertical move and mouse button dependant 2020
-
     public float mainSpeed = 80.0f; //regular speed
     public float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
     public float maxShift = 1000.0f; //Maximum speed when holdin gshift
@@ -22,22 +20,30 @@ public class MoveCamera : MonoBehaviour
     public Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
 
+    public GameObject authenticationWindow;
+
+
     private void Start()
     {
         lastMouse = Input.mousePosition;
     }
 
+
     void Update()
     {
-        move();
+        // Make sure camera doesn't move when typing and having input window open
+        if (authenticationWindow.activeInHierarchy) return;
+
+        Move();
 
         if (Input.GetMouseButton(0))
         {
-            rotateCamera();
+            RotateCamera();
         }
             
         lastMouse = Input.mousePosition;
     }
+
 
     private Vector3 GetBaseInput()
     { //returns the basic values, if it's 0 than it's not active.
@@ -69,7 +75,8 @@ public class MoveCamera : MonoBehaviour
         return p_Velocity;
     }
 
-    public void rotateCamera()
+
+    public void RotateCamera()
     {
         // Avoid click/drag through UI windows
         if (EventSystem.current.IsPointerOverGameObject())
@@ -85,36 +92,14 @@ public class MoveCamera : MonoBehaviour
         //Mouse  camera angle done.  
     }
 
-    public void move()
+
+    public void Move()
     {
         //Keyboard commands
         Vector3 p = GetBaseInput();
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            totalRun += Time.deltaTime;
-            p = p * totalRun * shiftAdd;
-            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-        }
-        else
-        {
-            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
-        }
-
-        p = p * Time.deltaTime;
-        Vector3 newPosition = transform.position;
-        if (Input.GetKey(KeyCode.Space))
-        { //If player wants to move on X and Z axis only
-            transform.Translate(p);
-            newPosition.x = transform.position.x;
-            newPosition.z = transform.position.z;
-            transform.position = newPosition;
-        }
-        else
-        {
-            transform.Translate(p);
-        }
+        totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+        p = p * mainSpeed * Time.deltaTime;
+        transform.Translate(p);
     }
+
 }
